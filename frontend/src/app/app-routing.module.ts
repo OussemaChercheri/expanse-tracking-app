9d61b6e04fe69b4f55451612e624d55c7d0fdc6c
Oussema Chercheri <oussama.chercheri@tek-up.de>
2024-05-14 13:11:17 +0100

    add Home UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {path:"Login", component: LoginComponent},
-  {path:"register", component: RegisterComponent}
+  {path:"register", component: RegisterComponent},
+  {path: "home", component: HomeComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './views/home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidenavComponent } from './layouts/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ForgetPasswordComponent
+    ForgetPasswordComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
